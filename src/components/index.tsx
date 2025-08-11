6a0a5a7be43f4a7c9d1a3533bb801c7bc5c19d70
Tarun Kondraju <84043703+TarunKondraju@users.noreply.github.com>
2025-08-11 11:34:11 +0530

    Create index.tsx

---
+++ src/components/index.tsx
@@ -0,0 +1,10 @@
+import ModelForm from "../components/ModelForm";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Earth Agri Guide</h1>
+      <ModelForm />
+    </div>
+  );
+}
